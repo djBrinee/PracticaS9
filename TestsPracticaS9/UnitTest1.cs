27f1cf60d839713736b4d46a0d53f8cb6ea60b21
Leonel Rodríguez <81274025+LeonelRod@users.noreply.github.com>
2023-01-05 21:19:25 -0400

    Test case para inscribir

--- TestsPracticaS9/UnitTest1.cs
+++ TestsPracticaS9/UnitTest1.cs
@@ -4,7 +4,7 @@ namespace TestsPracticaS9
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestAprobadaTrue()
         {
             Asignatura asignatura = new Asignatura()
             {
@@ -16,11 +16,47 @@ namespace TestsPracticaS9
                 asignatura = asignatura
             };
 
-           bool aprobada = calificaciones.Aprobada();
+            bool aprobada = calificaciones.Aprobada();
             
-            Assert.True(aprobada);   
-          
-            
+            Assert.True(aprobada);           
+        }
+        [Fact]
+        public void TestAprobadaFalse()
+        {
+            Asignatura asignatura = new Asignatura()
+            {
+                codigo = "codigo",
+                nombre = "nombre",
+                creditos = 6
+            };
+            Calificaciones calificaciones = new Calificaciones()
+            {
+                puntuacion = 60,
+                asignatura = asignatura
+            };
+
+            bool aprobada = calificaciones.Aprobada();
+
+            Assert.False(aprobada);
+        }
+
+        [Fact]
+        public void TestInscribirse()
+        {
+            List<Asignatura> listaAsignaturas = new List<Asignatura>();
+            List<Calificaciones> listaCalificaciones= new List<Calificaciones>();
+            Estudiante estudiante = new Estudiante()
+            {
+                asignaturas = listaAsignaturas,Nombre= "Hugo", Apellido="Chavez",calificaciones = listaCalificaciones,carrera= "Ingenieria de Software", creditosMaximos = 20, Edad = 21   
+            };
+            Asignatura asignatura = new Asignatura()
+            {
+                codigo = "codigo",
+                nombre = "nombre",
+                creditos = 6
+            };
+            int countAsignaturas = estudiante.inscribir(asignatura);
+            Assert.True(countAsignaturas == 1);
         }
     }
 }
