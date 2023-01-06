e67348cc09647dfb2030f285344f2b1442b02690
djBrinee <deivyjunior12@gmail.com>
2023-01-05 20:33:46 -0400

    Add project files.

---
+++ PracticaS9/Models/Calificaciones.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaS9.Models
+{
+    internal class Calificaciones
+    {
+        public int puntuacion { get; set; }
+        public Asignatura? asignatura { get; set; }
+    }
+
+
+}
---
+++ PracticaS9/Models/Profesor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaS9.Models
+{
+    internal class Profesor : Persona
+    {
+        public string Area { get; set; }
+    }
+}
